7f0a48f72d0fc44a77f3cd51f1c46ffbc37355bf
iKelvvv <kelvinlee@live.co.uk>
2021-11-27 13:14:44 +0000

    Create bookings model and make migrations to databse

--- restaurant_booking/models.py
+++ restaurant_booking/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+# Import Django authentication user system
+from django.contrib.auth.models import User
+#Import cloudinary for featured image
+from cloudinary.models import CloudinaryField
+
 # Create your models here.
+STATUS = ((0, "Booking Requested"), (1, "Booking Accepted"))
+
+class Booking(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    booking_date = models.DateField(auto_now=False)
+    booking_time = models.TimeField(auto_now=False)
+    booking_comments = models.TextField()
+    guest_count = models.IntegerField()
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    # Django magic method to return string represention of an object
+    def __str__(self):
+        return self.title
