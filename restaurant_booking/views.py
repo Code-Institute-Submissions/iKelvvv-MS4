39e1e6b011adecb7b04d2ce7c30856785b1ee602
iKelvvv <kelvinlee@live.co.uk>
2021-11-27 15:22:29 +0000

    Add booking list view class to views.py

--- restaurant_booking/views.py
+++ restaurant_booking/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+# Import Django generic libary
+from django.views import generic
+# Import Booking model from models
+from .models import Booking
 
 # Create your views here.
+class BookingList(generic.ListView):
+    model = Booking
+    template_name = "index.html"
+    paginate_by = 6
