c8b4cc75dd56e590fe75cd274a98f61104bfeb95
iKelvvv <kelvinlee@live.co.uk>
2021-11-27 15:03:05 +0000

    Add filter list, list view, search fields, approve bookings features to booking admin

--- restaurant_booking/admin.py
+++ restaurant_booking/admin.py
@@ -3,4 +3,17 @@ from django.contrib import admin
 from .models import Booking
 
 # Register your models here.
-admin.site.register(Booking)
+# admin.site.register(Booking)
+# list_filter = ('status', 'booking_time', 'booking_date')
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'booking_time', 'booking_date')
+    readonly_fields = ('title',)
+    list_display = ('title', 'user_id', 'booking_date', 'booking_time', 'guest_count')
+    search_fields = ('title', 'user_id')
+    actions = ['approve_booking']
+
+    def approve_booking(self, request, queryset):
+        queryset.update(approve_booking=True)
--- restaurant_booking/models.py
+++ restaurant_booking/models.py
@@ -2,17 +2,19 @@ from django.db import models
 
 # Import Django authentication user system
 from django.contrib.auth.models import User
-#Import cloudinary for featured image
+# Import cloudinary for featured image
 from cloudinary.models import CloudinaryField
+
 
 # Create your models here.
 STATUS = ((0, "Booking Requested"), (1, "Booking Accepted"))
 
 class Booking(models.Model):
+    title = models.CharField(max_length=200, unique=True, default='Booking Request')
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     booking_date = models.DateField(auto_now=False)
     booking_time = models.TimeField(auto_now=False)
-    booking_comments = models.TextField()
+    booking_comments = models.TextField(max_length=200, blank=True, default='Please note any allergies')
     guest_count = models.IntegerField()
     status = models.IntegerField(choices=STATUS, default=0)
 
