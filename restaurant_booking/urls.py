c30a0bc8f076f275b42b55b9b4a98a42a52d1b87
iKelvvv <kelvinlee@live.co.uk>
2021-12-11 14:24:11 +0000

    Add templates for manage booking and edit profile pages

--- restaurant_booking/urls.py
+++ restaurant_booking/urls.py
@@ -5,7 +5,8 @@ from django.urls import path
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('menu/', views.MenuView.as_view(), name='menu'),
-    path('my_booking_details/', views.BookingDetailsView.as_view(), name='booking_detail'),
     path('contact_us/', views.ContactView.as_view(), name='contact'),
     path('online_bookings/', views.OnlineBookingView.as_view(), name='online_booking'),
+    path('edit_profile/', views.EditProfile.as_view(), name='edit_profile'),
+    path('manage_booking/', views.ManageBooking.as_view(), name='manage_booking'),
 ]
--- restaurant_booking/views.py
+++ restaurant_booking/views.py
@@ -4,26 +4,34 @@ from django.views import generic
 # Import Booking model from models
 from .models import Booking
 
+
 # Create your views here.
 class HomeView(generic.ListView):
     model = Booking
     template_name = "index.html"
-    paginate_by = 6
+
 
 class MenuView(generic.ListView):
     model = Booking
     template_name = "menu.html"
 
 
-class BookingDetailsView(generic.ListView):
-    model = Booking
-    template_name = "booking_details.html"
-
-
 class ContactView(generic.ListView):
     model = Booking
     template_name = "contact.html"
 
+
 class OnlineBookingView(generic.ListView):
     model = Booking
     template_name = "online_booking.html"
+
+
+class EditProfile(generic.ListView):
+    model = Booking
+    template_name = "edit_profile.html"
+
+
+class ManageBooking(generic.ListView):
+    model = Booking
+    template_name = "manage_booking.html"
+
