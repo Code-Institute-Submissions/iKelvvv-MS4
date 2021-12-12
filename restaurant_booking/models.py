2fcb4ad21c741a99791e402e7e67bbbc6d3bd601
iKelvvv <kelvinlee@live.co.uk>
2021-12-12 12:44:31 +0000

    Add date created on and display bookings in descending order

--- restaurant_booking/models.py
+++ restaurant_booking/models.py
@@ -11,12 +11,16 @@ STATUS = ((0, "Booking Requested"), (1, "Booking Accepted"))
 
 class Booking(models.Model):
     title = models.CharField(max_length=200, unique=True, default='Booking Request')
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_bookings")
     booking_date = models.DateField(auto_now=False)
     booking_time = models.TimeField(auto_now=False)
     booking_comments = models.TextField(max_length=200, blank=True, default='Please note any allergies')
+    created_on = models.DateTimeField(auto_now_add=True)
     guest_count = models.IntegerField()
     status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ['-created_on']
 
     # Django magic method to return string represention of an object
     def __str__(self):
