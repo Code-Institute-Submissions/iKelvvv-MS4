c11d981cf808650b669f6cbb0c1ebf534af3e7cc
iKelvvv <kelvinlee@live.co.uk>
2021-12-13 08:40:05 +0000

    Convert generic list views to template views

--- restaurant_booking/views.py
+++ restaurant_booking/views.py
@@ -1,37 +1,41 @@
 from django.shortcuts import render
 # Import Django generic libary
-from django.views import generic
+from django.views import generic, View
+from django.views.generic import TemplateView
 # Import Booking model from models
 from .models import Booking
 
 
 # Create your views here.
-class HomeView(generic.ListView):
-    model = Booking
+class HomeView(TemplateView):
     template_name = "index.html"
 
 
-class MenuView(generic.ListView):
-    model = Booking
+class MenuView(TemplateView):
     template_name = "menu.html"
 
 
-class ContactView(generic.ListView):
-    model = Booking
+class ContactView(TemplateView):
     template_name = "contact.html"
 
 
-class OnlineBookingView(generic.ListView):
-    model = Booking
+class OnlineBookingView(TemplateView):
     template_name = "online_booking.html"
 
 
-class EditProfile(generic.ListView):
-    model = Booking
+class EditProfile(TemplateView):
     template_name = "edit_profile.html"
 
 
-class ManageBooking(generic.ListView):
+class ManageBooking(generic.ListView): 
     model = Booking
     template_name = "manage_booking.html"
+    paginate_by = 6
 
+    def get_booking_data(request):
+        bookings = Booking.objects.all()
+        context = {
+            'bookings': bookings
+        }
+        return render(request, 'manage_booking/', context)
+        
