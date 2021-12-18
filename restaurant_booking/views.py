6b0b31a74fd9f35373247243788c7c2bc7772edd
iKelvvv <kelvinlee@live.co.uk>
2021-12-18 15:21:47 +0000

    Add method to create an online booking

--- restaurant_booking/views.py
+++ restaurant_booking/views.py
@@ -6,7 +6,7 @@ from django.views.generic import TemplateView, DetailView
 # Import Booking model from models
 from .models import Booking
 from .forms import UpdateBookingDetails
-
+from django.shortcuts import redirect
 
 # Create your views here.
 class HomeView(TemplateView):
@@ -21,10 +21,6 @@ class ContactView(TemplateView):
     template_name = "contact.html"
 
 
-class OnlineBookingView(TemplateView):
-    template_name = "online_booking.html"
-
-
 class EditProfile(TemplateView):
     template_name = "edit_profile.html"
 
@@ -37,6 +33,35 @@ class ManageBooking(generic.ListView):
 
     def get_queryset(self): 
         return Booking.objects.filter(user_id=self.request.user)
+
+
+class OnlineBookingView(View):
+    template_name = "online_booking.html"
+
+    def get(self, request, *args, **kwargs):
+        return render(
+            request,
+            "online_booking.html",
+        )
+
+    def post(self, request):
+        date = request.POST.get("date")
+        time = request.POST.get("time")
+        guest_count = request.POST.get("guest_count")
+        comments = request.POST.get("comments")
+
+        online_booking = Booking.objects.create(
+            booking_date=date,
+            booking_time=time,
+            guest_count=guest_count,
+            user=request.user,
+            booking_comments=comments
+        )
+
+        online_booking.save()
+
+        return redirect(reverse('manage_booking'))
+
 
 class EditBooking(View):
     model = Booking
