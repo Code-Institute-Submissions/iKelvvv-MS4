acb11ec113ffab2f2296d7d8b0ee346b53a636f8
iKelvvv <kelvinlee@live.co.uk>
2021-12-19 15:19:24 +0000

    Swap booking fields order so that guest_count would show above booking_comments

--- restaurant_booking/forms.py
+++ restaurant_booking/forms.py
@@ -5,4 +5,4 @@ from django import forms
 class UpdateBookingDetails(forms.ModelForm):
     class Meta:
         model = Booking
-        fields = ('booking_date', 'booking_time', 'booking_comments', 'guest_count',)
+        fields = ('booking_date', 'booking_time', 'guest_count', 'booking_comments',)
