d6bbbe22956fcdbebe8425551e93aefaf34bcfd6
iKelvvv <kelvinlee@live.co.uk>
2021-12-21 18:56:08 +0000

    Add content to the UX section for the README.md file

--- restaurant_booking/forms.py
+++ restaurant_booking/forms.py
@@ -1,8 +1,15 @@
-from .models import Booking
+from .models import Booking, UserProfile
 from django import forms
+from django.forms import ModelForm
 
 
 class UpdateBookingDetails(forms.ModelForm):
     class Meta:
         model = Booking
         fields = ('booking_date', 'booking_time', 'guest_count', 'booking_comments',)
+
+
+class EditProfileForm(ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('first_name', 'last_name', 'phone_number')
