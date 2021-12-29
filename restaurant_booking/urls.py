d5472fa6a250bc4ea9098b296670854acfa6480e
iKelvvv <kelvinlee@live.co.uk>
2021-12-29 18:22:22 +0000

    Rename online_booking path in the URLs

--- restaurant_booking/urls.py
+++ restaurant_booking/urls.py
@@ -6,9 +6,10 @@ urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('menu/', views.MenuView.as_view(), name='menu'),
     path('contact_us/', views.ContactView.as_view(), name='contact'),
-    path('online_bookings/', views.OnlineBookingView.as_view(), name='online_booking'),
+    path('online_booking/', views.OnlineBookingView.as_view(), name='online_booking'),
     path('create_profile/', views.CreateProfile.as_view(), name='create_profile'),
     path('edit_profile/<str:user>/', views.EditProfile.as_view(), name='edit_profile'),
     path('manage_booking/', views.ManageBooking.as_view(), name='manage_booking'),
     path('edit_booking/<str:booking_id>/', views.EditBooking.as_view(), name='edit_booking'),
+    path('delete_booking/<str:booking_id>/', views.DeleteBooking.as_view(), name='delete_booking'),
 ]
